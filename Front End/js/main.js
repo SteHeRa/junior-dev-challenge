56802387ce7d45d5dab917bab5037cb5547c58da
Ste Head-Rapson <stephenheadrapson@hotmail.co.uk>
2020-11-09 12:19:20 +0000

    feat: showing clients & redirect on click

--- Front End/js/main.js
+++ Front End/js/main.js
@@ -1,30 +1,48 @@
-
 var map;
 function addPinsOnMap(latLongs) {
-	var myIcon = L.divIcon({html: '<img height="50px" width="50px" src="pictures/pin.png"></img>'})
-	for (var i = 0; i < latLongs.length; i++) {
-		latLongs[i]
-	  var marker2 = L.marker([latLongs[i].lat, latLongs[i].lon], {icon: myIcon}).addTo(map);
-		  marker2.bindPopup(latLongs[i].name);
-		  marker2.on('mouseover', function (e) {
-		    this.openPopup();
-	  });
-	}
-}
-$(document).ready(function() {
-	map = new L.Map('map', { zoom: 15});
-
-	// create the tile layer with correct attribution
-	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-	var osm = new L.TileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-        minZoom: 2,
-        maxZoom: 20,
-        maxNativeZoom: 17
+  var myIcon = L.divIcon({
+    html: '<img height="50px" width="50px" src="pictures/pin.png"></img>',
+  });
+  for (var i = 0; i < latLongs.length; i++) {
+    latLongs[i];
+    var marker2 = L.marker([latLongs[i].lat, latLongs[i].lon], {
+      icon: myIcon,
+    }).addTo(map);
+    marker2.bindPopup(`
+				Client Name: ${latLongs[i].name} <br>
+				`);
+    marker2.on('mouseover', function () {
+      this.openPopup();
     });
-	map.setView(new L.LatLng(51.4839337, -0.2549888),9);
-	map.addLayer(osm);
-  map.invalidateSize();
-	addPinsOnMap([{lat:51.5494457,lon:-0.4487664,name:'Cordant Hillingdon'}])
+    marker2.on('click', function () {
+      fetch(`http://localhost:3000/client?${latLongs[i].name}`);
+    });
+  }
+}
+$(document).ready(function () {
+  map = new L.Map('map', { zoom: 15 });
+
+  // create the tile layer with correct attribution
+  var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  var osmAttrib =
+    'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+  var osm = new L.TileLayer(
+    'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+    {
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+      minZoom: 2,
+      maxZoom: 20,
+      maxNativeZoom: 17,
+    }
+  );
+  (async () => {
+    map.setView(new L.LatLng(52.482793, -1.885955), 11);
+    map.addLayer(osm);
+    map.invalidateSize();
+    const clients = await fetch('http://localhost:3000/clients').then((res) =>
+      res.json()
+    );
+    addPinsOnMap(clients);
+  })();
 });
